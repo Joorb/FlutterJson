cbb6672e118d4216f9c64ce86adf387e557de37d
Joorb <joorb411@gmail.com>
2025-03-26 11:59:51 -0300

    Primeira atualização funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:app_jason_jrl/filmes.dart'; // import para usar o json
+import 'package:app_jason_jrl/filmes.dart';
 import 'dart:convert'; 
 import 'package:flutter/services.dart';
 
@@ -12,10 +12,10 @@ class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
-  _MainAppState createState() => _MainAppState(); // Definir a classe de estado
+  _MainAppState createState() => _MainAppState();
 }
 
-class _MainAppState extends State<MainApp> { // Alterei o nome da classe de estado para corresponder
+class _MainAppState extends State<MainApp> {
   bool mostrarFilme = false;
   List<Filmes> filmes = List.empty();
   Filmes filmeSorteado = Filmes();
@@ -26,7 +26,7 @@ class _MainAppState extends State<MainApp> { // Alterei o nome da classe de esta
     Iterable data = await json.decode(response);
     filmes = List<Filmes>.from(data.map((model) => Filmes.fromJson(model)));
     total = filmes.length;
-    setState(() {}); // Atualiza a interface
+    setState(() {});
   }
 
   void sorteiaFilme() {
@@ -40,7 +40,7 @@ class _MainAppState extends State<MainApp> { // Alterei o nome da classe de esta
   @override
   void initState() {
     super.initState();
-    readJson(); // Carregar os filmes ao iniciar
+    readJson();
   }
 
   @override
@@ -54,13 +54,18 @@ class _MainAppState extends State<MainApp> { // Alterei o nome da classe de esta
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      'Filme Sorteado: ${filmeSorteado.titulo}', // Supondo que o campo seja 'titulo' no seu modelo Filmes
+                      'Filme Sorteado: ${filmeSorteado.titulo}',
                       style: TextStyle(fontSize: 24),
                     ),
                     Text(
                       'Total de Filmes: $total',
                       style: TextStyle(fontSize: 18),
                     ),
+                    SizedBox(height: 5),
+                    ElevatedButton(
+                      onPressed: sorteiaFilme,
+                      child: const Text('Sortear Novo Filme'),
+                    )
                   ],
                 )
                 
